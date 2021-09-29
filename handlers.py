fdf36cb656e4bba27cda849f42baba766cc39d3c
YernurYbrayev <70425126+YernurYbrayev@users.noreply.github.com>
2021-09-29 09:32:38 +0600

    Mistake deleted

--- handlers.py
+++ handlers.py
@@ -4,7 +4,7 @@ from aiogram.types import Message
 from config import admin_id
 
 async def send_to_admin(dp):
-    await bot.send_message(chat_id = admin_id, text="Bot started")
+    await bot.send_message(chat_id = admin_id, text="Bot started!")
 
 @dp.message_handler()
 async def echo(message: Message):
